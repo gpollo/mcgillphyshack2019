b18858773a04ce05aa9e9c2fa9bb625ac4272905
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-11-02 14:28:04 -0400

    Add game into window

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from sys import argv, exit
 from widget.plot import PlotWidget
+from widget.surface import SurfaceWidget
+from game import Game
+from pygame import init as pygame_init
 
 from PyQt5.QtWidgets import QWidget, QApplication, QSplitter, QHBoxLayout, QLabel
 from PyQt5.QtCore import Qt
 
 class MainWindow(QWidget):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
 
-        self.__widget_left = PlotWidget()
+        self.__game = game
+        self.__widget_left = SurfaceWidget(self.__game.get_surface())
         self.__widget_right = PlotWidget()
+        self.__game.start(self.__widget_left)
 
         self.__splitter = QSplitter(Qt.Horizontal)
         self.__splitter.addWidget(self.__widget_left)
@@ -19,8 +24,13 @@ class MainWindow(QWidget):
         self.__layout.addWidget(self.__splitter)
         self.setLayout(self.__layout)
 
+pygame_init()
+game = Game()
+
 app = QApplication(argv)
-window = MainWindow()
+window = MainWindow(game)
 window.setWindowTitle("test")
 window.show()
-exit(app.exec_())
+
+app.exec_()
+game.stop()

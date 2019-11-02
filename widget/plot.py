765c934cf05b42c109ab6e5d5c0c505675c14a8f
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-11-02 14:36:49 -0400

    Setup transparent plot

--- widget/plot.py
+++ widget/plot.py
@@ -8,8 +8,13 @@ class PlotWidget(QWidget):
         super().__init__()
 
         self.__canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self.__canvas.setStyleSheet("background-color:transparent;")
         self.__axes = self.__canvas.figure.subplots()
         self.__axes.plot([0,1,2,3,4], [0,1,0,1,0])
+
+        self.__axes.figure.patch.set_alpha(0.5)
+        self.__axes.figure.set_facecolor("None")
+        self.__axes.patch.set_alpha(0.0)
 
         self.__layout = QHBoxLayout(self)
         self.__layout.addWidget(self.__canvas)
